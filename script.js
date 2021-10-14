9cec0838abe910224a85d6738ea85dc0efde098b
Surbhi Mathur <67837866+surbhimathur@users.noreply.github.com>
2021-10-14 19:57:08 +0530

    callbackhell task

---
+++ script.js
@@ -0,0 +1,43 @@
+setTimeout(() => {
+  document.querySelector(".countdown").innerHTML = 10;
+  setTimeout(() => {
+    document.querySelector(".countdown").innerHTML = 9;
+
+    setTimeout(() => {
+      document.querySelector(".countdown").innerHTML = 8;
+
+      setTimeout(() => {
+        document.querySelector(".countdown").innerHTML = 7;
+
+        setTimeout(() => {
+          document.querySelector(".countdown").innerHTML = 6;
+
+          setTimeout(() => {
+            document.querySelector(".countdown").innerHTML = 5;
+
+            setTimeout(() => {
+              document.querySelector(".countdown").innerHTML = 4;
+
+              setTimeout(() => {
+                document.querySelector(".countdown").innerHTML = 3;
+
+                setTimeout(() => {
+                  document.querySelector(".countdown").innerHTML = 2;
+
+                  setTimeout(() => {
+                    document.querySelector(".countdown").innerHTML = 1;
+
+                    setTimeout(() => {
+                      document.querySelector(".countdown").innerHTML =
+                        "Happy Independance day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
